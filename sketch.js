7d008bad2b5f92fcf827488d48209b2c9773e250
eclefley <eclefley@gmail.com>
2021-12-21 14:08:13 -0600

    merge

--- sketch.js
+++ sketch.js
@@ -18,11 +18,69 @@ let img2;
 let img3;
 let img4;
 
-let sound1;
-let sound2;
-let sound3;
-let sound4;
+let darkestPoint;
 
+let zoneOne = {
+    box: {
+        x: 0,
+        y: 0,
+        width: 50,
+        height: 50,
+    },
+    isActive: false,
+    name: 'LK1',
+};
+
+let zoneTwo = {
+    box: {
+    x: 0,
+    y: 0,
+    width: 770,
+    height: 590,
+    },
+    isActive: false,
+    name: 'LK2',
+};
+
+let zoneThree = {
+    box: {
+    x: 0,
+    y: 0,
+    width: 770,
+    height: 5,
+    },
+    isActive: false,
+    name: 'LK3',
+};
+
+let zoneFour = {
+    box: {
+    x: 0,
+    y: 0,
+    width: 50,
+    height: 590,
+    },
+    isActive: false,
+    name: 'LK4',
+};
+
+let zoneArray = [zoneOne, zoneTwo, zoneThree, zoneFour];
+
+//sounds
+var len;
+let randSound;
+let bellSounds = [];
+let dingSounds = [];
+let bassSounds = [];
+let singingSounds = [];
+let meditationVoiceLoop;
+
+//let sound1;
+//let sound2;
+//let sound3;
+//let sound4;
+
+/*
 function preload() {
   img1 = loadImage('meditation1.jpg');
   img2 = loadImage('meditation2.jpg');
@@ -33,6 +91,64 @@ function preload() {
   sound3 = loadSound('voice3.mp3');
   sound4 = loadSound('voice4.mp3');
 }
+*/
+
+function playRandomSound(sounds, volume) {
+    randSound = random(sounds);
+    randSound.setVolume(volume);
+    
+    if(!randSound.isPlaying() && randSound.isLoaded()) {
+        randSound.play();
+        console.log("playing")
+    }  
+}
+
+function activateZone(zone) {
+    zone.isActive = true;
+    
+    if(zone == zoneOne) {
+        playRandomSound(bellSounds, 0.1);
+    }
+    
+    if(zone == zoneTwo) {
+        playRandomSound(singingSounds, 0.1);
+    }
+    
+    if(zone == zoneThree) {
+        playRandomSound(dingSounds, 0.8);
+    }
+    
+    if(zone == zoneFour) {
+        playRandomSound(bassSounds, 0.5);
+    }
+    
+    print(zone.name, " was just activated");
+}
+
+function deactivateZone (zone) {
+    zone.isActive = false;
+    print(zone.name, " was just de-activated");
+}
+
+function isDarkestPointInZone(zone) {
+    if(darkestPoint.x < zone.box.width+zone.box.x && darkestPoint.y < zone.box.height+zone.box.y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function queueVoiceLoopAndPlayVoiceImages() {
+    console.log("starting voice images");
+    meditationVoiceImages.onended(queueVoiceImagesAndPlayVoiceLoop);
+    meditationVoiceImages.play();
+}
+
+function queueVoiceImagesAndPlayVoiceLoop() {
+    console.log("starting voice loop");
+    meditationVoiceLoop.onended(queueVoiceLoopAndPlayVoiceImages);
+    meditationVoiceLoop.play();
+}
 
 function setup() {
   createCanvas(3840, 1080);
@@ -42,10 +158,28 @@ function setup() {
   myCapture.hide();
   // wait for OpenCV to init
   p5.cv.onComplete = onOpenCVComplete;
-  sound1.playMode('untilDone');
-  sound2.playMode('untilDone');
-  sound3.playMode('untilDone');
-  sound4.playMode('untilDone');
+  //sound1.playMode('untilDone');
+  //sound2.playMode('untilDone');
+  //sound3.playMode('untilDone');
+  //sound4.playMode('untilDone');
+    
+    bellSounds.push(loadSound("bell_C.wav"));
+    bellSounds.push(loadSound("bell_FS.wav"));
+    bellSounds.push(loadSound("bell_A.wav"));
+    
+    singingSounds.push(loadSound("voice1.mp3"));
+    singingSounds.push(loadSound("voice2.mp3"));
+    singingSounds.push(loadSound("voice3.mp3"));
+    singingSounds.push(loadSound("voice4.mp3")); 
+    
+    dingSounds.push(loadSound("ding_AS.wav"));
+    dingSounds.push(loadSound("ding_FS.wav"));
+    dingSounds.push(loadSound("ding_DS.wav"));
+    
+    bassSounds.push(loadSound("bassInOut.wav"));
+    
+    meditationVoiceLoop = loadSound("voice_loop.wav", queueVoiceImagesAndPlayVoiceLoop);
+    meditationVoiceImages = loadSound("voice_with_images.wav");
 }
 
 function onOpenCVComplete() {
@@ -57,6 +191,9 @@ function onOpenCVComplete() {
   myMatGrayscale = new cv.Mat();
 }
 
+let i;
+let zone;
+
 function draw() {
   background(0);
   if (p5.cv.isReady) {
@@ -67,9 +204,11 @@ function draw() {
     // display Mat
     p5.cv.drawMat(myMatGrayscale, 0, 0);
     // get brightnest point
-    let brightestPoint = p5.cv.findMinLocation(myMatGrayscale);
+    darkestPoint = p5.cv.findMinLocation(myMatGrayscale);
     // draw brightest point
-    circle(brightestPoint.x, brightestPoint.y, 30);
+    //circle(brightestPoint.x, brightestPoint.y, 30);
+      
+      /*
 
     if(brightestPoint.x < 320 && brightestPoint.y < 240) {
       print("LK1");
@@ -90,5 +229,16 @@ function draw() {
     }
   } else {
     image(myCapture, 0, 0);
+  }
+  */
+      for (i = 0; i < zoneArray.length; i+=1) {
+            zone = zoneArray[i];
+
+            if(isDarkestPointInZone(zone) && zone.isActive == false) {
+                activateZone(zone);
+            } else if(!isDarkestPointInZone(zone) && zone.isActive == true) {
+                deactivateZone(zone);
+            }
+        }
   }
 }
